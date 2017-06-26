264ac831d8b35e03f4955143f4494c38b8f46f19
Brian Hopkins <brianhh1230@gmail.com>
2017-06-26 17:35:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 
 setup(
-    name="Mattermost-API",
+    name="mattermost_wrapper",
     version="1.0.0",
     author="Brian Hopkins",
     author_email="btotharye@gmail.com",
     description=("A mattermost api v4 wrapper to interact with api"),
     license="MIT",
-    packages=['wrapper'],
+    packages=[''],
 )
