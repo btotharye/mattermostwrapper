f6656abb8bdfa302872cd01606b42d91871167d7
Brian Hopkins <brianhh1230@gmail.com>
2017-06-26 17:42:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from distutils.core import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name="mattermost_wrapper",
+    name="Mattermost-API",
     version="1.0.0",
     author="Brian Hopkins",
     author_email="btotharye@gmail.com",
     description=("A mattermost api v4 wrapper to interact with api"),
+    long_description=long_description,
     license="MIT",
-    packages=[''],
+    packages=find_packages(),
+    install_requires=['requests'],
 )
+
