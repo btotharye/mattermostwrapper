9c5020bbc970eb53b83746496be82b96b288c3d9
Brian Hopkins <brianhh1230@gmail.com>
2017-06-26 17:19:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
 from distutils.core import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-
-
-def long_description():
-    with codecs.open('README.md', encoding='utf8') as f:
-        return f.read()
-
 setup(
     name="Mattermost-API",
     version="1.0.0",
     author="Brian Hopkins",
     author_email="btotharye@gmail.com",
     description=("A mattermost api v4 wrapper to interact with api"),
-    long_description=long_description,
     license="MIT",
     packages=[''],
 )
