cffd4cc44e7c132cc2ed470f3f40d909392540d0
Brian Hopkins <brianhh1230@gmail.com>
2017-06-26 20:02:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description=("A mattermost api v4 wrapper to interact with api"),
     long_description=long_description,
     license="MIT",
-    packages=['wrapper'],
+    packages=find_packages(),
     install_requires=['requests'],
 )
 
