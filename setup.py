7592728845ba99785ebaa36896bc7b8937b09f6a
Brian Hopkins <brianhh1230@gmail.com>
2018-03-31 07:25:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     version="0.1",
     author="Brian Hopkins",
     author_email="btotharye@gmail.com",
-    url='https://github.com/btotharye/mattermost_wrapper.git',
-    download_url='https://github.com/btotharye/mattermost_wrapper/archive/0.1.tar.gz',
+    url='https://github.com/btotharye/mattermostwrapper.git',
+    download_url='https://github.com/btotharye/mattermostwrapper/archive/0.1.tar.gz',
     description=("A mattermost api v4 wrapper to interact with api"),
     license="MIT",
     install_requires=[
